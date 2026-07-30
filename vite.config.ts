298b0357b2bcb350bbf71690aa0f4d1c344344b1
rustamkremcheev <rustam.omg1996@gmail.com>
2026-07-30 13:44:58 -0400

    Configure Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/java-interview-quiz/',
+  
   plugins: [
     react(),
     VitePWA({
@@ -15,6 +17,8 @@ export default defineConfig({
         theme_color: '#0b0f19',
         background_color: '#0b0f19',
         display: 'standalone',
+		start_url: '/java-interview-quiz/',
+        scope: '/java-interview-quiz/',
         icons: [
           {
             src: 'pwa-192x192.png',
